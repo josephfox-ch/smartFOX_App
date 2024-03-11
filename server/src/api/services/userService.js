83f75f29d8d679eff35c18a779ce7fcd5c19ac2e
josephfox-ch <142756034+josephfox-ch@users.noreply.github.com>
2024-03-11 13:50:22 +0100

    "fix: Solve Docker and Bcrypt conflict and add some validations"

--- server/src/api/services/userService.js
+++ server/src/api/services/userService.js
@@ -2,8 +2,7 @@ import User from "../models/user.js";
 import bcrypt from "bcryptjs";
 
 const createUser = async (userData) => {
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(userData.password, salt);
+  const hashedPassword = await bcrypt.hash(userData.password, 12);
   const user = await User.create({ ...userData, password: hashedPassword });
   return user;
 };
@@ -14,19 +13,23 @@ const getAllUsers = async () => {
 };
 
 const updateUser = async (id, userData) => {
-  const user = await User.update(userData, {
-    where: {
-      id: id,
-    },
+  if (userData.password) {
+    userData.password = await bcrypt.hash(userData.password, 12);
+  }
+  const [updated] = await User.update(userData, {
+    where: { id },
   });
-  return user;
+  if (updated) {
+    const updatedUser = await User.findByPk(id);
+    return updatedUser;
+  }
+
+  throw new Error("User not found");
 };
 
 const deleteUser = async (id) => {
   const user = await User.destroy({
-    where: {
-      id: id,
-    },
+    where: { id },
   });
   return user;
 };
