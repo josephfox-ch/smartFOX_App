8dd745c0dabec9cab9500dc07e99e648c2084c72
josephfox-ch <142756034+josephfox-ch@users.noreply.github.com>
2024-03-11 11:28:21 +0100

    "build: import bcrypt to hash password"

--- server/src/api/services/userService.js
+++ server/src/api/services/userService.js
@@ -1,7 +1,10 @@
 import User from "../models/user.js";
+import bcrypt from "bcryptjs";
 
 const createUser = async (userData) => {
-  const user = await User.create(userData);
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(userData.password, salt);
+  const user = await User.create({ ...userData, password: hashedPassword });
   return user;
 };
 
@@ -10,7 +13,7 @@ const getAllUsers = async () => {
   return users;
 };
 
-const updateUser = async (id,userData) => {
+const updateUser = async (id, userData) => {
   const user = await User.update(userData, {
     where: {
       id: id,
