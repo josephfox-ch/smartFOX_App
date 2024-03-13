2aadfdbbfe6665035f4292262d8d39dc75593dbf
josephfox-ch <142756034+josephfox-ch@users.noreply.github.com>
2024-03-13 07:20:22 +0100

    "Build: refactor user controller layout and service layout, create UserHome model"

--- server/src/api/models/index.js
+++ server/src/api/models/index.js
@@ -1,11 +1,12 @@
 import User from "./user.js";
 import UserDetails from "./userDetails.js";
 import Home from "./home.js";
-
+import UserHome from './userHome.js'; 
 
 User.hasOne(UserDetails, {
   foreignKey: "userId", 
-  as: "UserDetails", 
+  as: "UserDetails",
+  onDelete: 'CASCADE', 
 });
 UserDetails.belongsTo(User, {
   foreignKey: "userId",
@@ -13,11 +14,11 @@ UserDetails.belongsTo(User, {
 });
 
 
-User.hasMany(Home, {
-  foreignKey: "userId", 
-  as: "Homes", 
-});
-Home.belongsTo(User, {
-  foreignKey: "userId",
-  as: "user",
-});
+User.belongsToMany(Home, { through: UserHome, as: 'homes', foreignKey: 'userId' });
+Home.belongsToMany(User, { through: UserHome, as: 'users', foreignKey: 'homeId' });
+
+export { User, UserDetails, Home, UserHome }; 
+
+
+
+
