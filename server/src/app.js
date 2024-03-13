68f7ce0088023702b960d27b719f5c4dc57ca454
josephfox-ch <142756034+josephfox-ch@users.noreply.github.com>
2024-03-13 07:58:01 +0100

    "Build: install passport,passport-jwt, jsonwebtoken packages"

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import userRoutes from './api/routes/userRoutes.js';
+import homeRoutes from './api/routes/homeRoutes.js';
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(express.static('public'));
 
 
 app.use('/api/v1/users', userRoutes);
+app.use('/api/v1/homes', homeRoutes);
 
 
 app.get('/', (req, res) => {
