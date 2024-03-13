271089d65041feaf81743e2699e8972a27820398
josephfox-ch <142756034+josephfox-ch@users.noreply.github.com>
2024-03-13 08:18:58 +0100

    "Build: create authRoutes.js"

--- server/src/app.js
+++ server/src/app.js
@@ -1,20 +1,25 @@
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
+import './config/passport.js'
 import userRoutes from './api/routes/userRoutes.js';
 import homeRoutes from './api/routes/homeRoutes.js';
+import authRoutes from './api/routes/authRoutes.js';
 
 const app = express();
 
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(passport.initialize());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 
+
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/homes', homeRoutes);
+app.use('/api/v1/auth',authRoutes);
 
 
 app.get('/', (req, res) => {
