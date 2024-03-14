5c4c5b548ce93c50a5d28ab48c7ce8ee8638c350
josephfox-ch <142756034+josephfox-ch@users.noreply.github.com>
2024-03-14 01:41:47 +0100

    Feat: Add isVerified property to User model refactor verifyOTP func.

--- server/src/api/services/authService.js
+++ server/src/api/services/authService.js
@@ -2,7 +2,11 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import { User } from "../models/index.js";
 import sendSMS from "../../notifications/smsService.js";
-import { generateOTP,saveOTPForUser,checkOTPForUser } from "../../utils/utils.js";
+import {
+  generateOTP,
+  saveOTPForUser,
+  checkOTPForUser,
+} from "../../utils/utils.js";
 
 const authService = {
   async register(userData) {
@@ -18,20 +22,34 @@ const authService = {
     return { id: user.id, email: user.email, otpSent: true };
   },
   async verifyOTP(userId, otpInput) {
-    const isValid = await checkOTPForUser(userId, otpInput);
-    if (!isValid) throw new Error("Invalid or expired OTP");
+    await checkOTPForUser(userId, otpInput);
+
+    await User.update({ isVerified: true }, { where: { id: userId } });
 
     const user = await User.findByPk(userId);
+    if (!user) throw new Error("User Not Found.");
+
     const payload = { userId: user.id, email: user.email };
+
     const token = jwt.sign(payload, process.env.JWT_SECRET, {
       expiresIn: "1w",
     });
 
-    return { token, user: { id: user.id, email: user.email } };
+    return {
+      token,
+      user: { id: user.id, email: user.email, isVerified: user.isVerified },
+    };
   },
+
   async login(email, password) {
     const user = await User.findOne({ where: { email } });
     if (!user) throw new Error("User not found.");
+
+    if (!user.isVerified) {
+      throw new Error(
+        "User is not yet verified. Please complete the 2FA verification."
+      );
+    }
 
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) throw new Error("Invalid Password.");
