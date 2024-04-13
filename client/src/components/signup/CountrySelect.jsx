6037d384eba9aee7dffce2b7384e9fdd37416168
josephfox-ch <142756034+josephfox-ch@users.noreply.github.com>
2024-04-13 15:28:24 +0200

    Build: CountrySelect component for signup form

---
+++ client/src/components/signup/CountrySelect.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import Select from 'react-select';
+import CountryFlag from 'react-country-flag';
+import countries from 'i18n-iso-countries';
+import englishCountries from 'i18n-iso-countries/langs/en.json';
+
+countries.registerLocale(englishCountries);
+
+const countryOptions = Object.entries(countries.getNames("en", { select: "official" })).map(([code, name]) => ({
+  value: code,
+  label: (
+    <div style={{ display: 'flex', alignItems: 'center' }}>
+      <CountryFlag countryCode={code} svg style={{ width: '20px', marginRight: '10px' }} />
+      {name}
+    </div>
+  ),
+  name: name.toLowerCase()
+}));
+
+const customStyles = {
+  control: (base) => ({
+    ...base,
+    minHeight: 40,
+    marginBottom: '10px',
+  }),
+  dropdownIndicator: (base) => ({
+    ...base,
+    padding: 4,
+  }),
+  clearIndicator: (base) => ({
+    ...base,
+    padding: 4,
+  }),
+  valueContainer: (base) => ({
+    ...base,
+    padding: '0px 10px',
+    
+  }),
+  input: (base) => ({
+    ...base,
+    margin: 0,
+    padding: 0,
+    
+  }),
+};
+
+const filterOption = ({ data }, inputValue) => data.name.includes(inputValue.toLowerCase());
+
+
+const CountrySelect = ({ onChange }) => (
+  <Select
+    options={countryOptions}
+    styles={customStyles}
+    placeholder="Select a country..."
+    onChange={onChange}
+    filterOption={filterOption}
+
+
+  />
+);
+
+export default CountrySelect;
+
