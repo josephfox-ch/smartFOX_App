693445c3fa91a0b1a9888c598ea4d1fc0f7a3969
josephfox-ch <142756034+josephfox-ch@users.noreply.github.com>
2024-04-13 16:16:02 +0200

    Build: Create TimeZoneSelect component

--- client/src/components/signup/CountrySelect.jsx
+++ client/src/components/signup/CountrySelect.jsx
@@ -1,27 +1,35 @@
-import React from 'react';
-import Select from 'react-select';
-import CountryFlag from 'react-country-flag';
-import countries from 'i18n-iso-countries';
-import englishCountries from 'i18n-iso-countries/langs/en.json';
+import React from "react";
+import Select from "react-select";
+import CountryFlag from "react-country-flag";
+import countries from "i18n-iso-countries";
+import englishCountries from "i18n-iso-countries/langs/en.json";
 
 countries.registerLocale(englishCountries);
 
-const countryOptions = Object.entries(countries.getNames("en", { select: "official" })).map(([code, name]) => ({
+const countryOptions = Object.entries(
+  countries.getNames("en", { select: "official" })
+).map(([code, name]) => ({
   value: code,
   label: (
-    <div style={{ display: 'flex', alignItems: 'center' }}>
-      <CountryFlag countryCode={code} svg style={{ width: '20px', marginRight: '10px' }} />
+    <div style={{ display: "flex", alignItems: "center" }}>
+      <CountryFlag
+        countryCode={code}
+        svg
+        style={{ width: "20px", marginRight: "10px" }}
+      />
       {name}
     </div>
   ),
-  name: name.toLowerCase()
+  name: name.toLowerCase(),
 }));
 
 const customStyles = {
   control: (base) => ({
     ...base,
     minHeight: 40,
-    marginBottom: '10px',
+    marginBottom: "10px",
+    border: "1px solid #ced4da",
+    borderRadius: 5,
   }),
   dropdownIndicator: (base) => ({
     ...base,
@@ -33,19 +41,17 @@ const customStyles = {
   }),
   valueContainer: (base) => ({
     ...base,
-    padding: '0px 10px',
-    
+    padding: "0px 10px",
   }),
   input: (base) => ({
     ...base,
     margin: 0,
     padding: 0,
-    
   }),
 };
 
-const filterOption = ({ data }, inputValue) => data.name.includes(inputValue.toLowerCase());
-
+const filterOption = ({ data }, inputValue) =>
+  data.name.includes(inputValue.toLowerCase());
 
 const CountrySelect = ({ onChange }) => (
   <Select
@@ -54,10 +60,7 @@ const CountrySelect = ({ onChange }) => (
     placeholder="Select a country..."
     onChange={onChange}
     filterOption={filterOption}
-
-
   />
 );
 
 export default CountrySelect;
-
