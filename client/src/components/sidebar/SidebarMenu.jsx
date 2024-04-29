b8d739e443afbbd9e1abe7cc28ab6a40e90b8b73
josephfox-ch <142756034+josephfox-ch@users.noreply.github.com>
2024-04-30 01:18:32 +0200

    Build: useKeydown Hook

--- client/src/components/sidebar/SidebarMenu.jsx
+++ client/src/components/sidebar/SidebarMenu.jsx
@@ -4,8 +4,7 @@ import SidebarLinkGroup from './SidebarLinkGroup';
 const SidebarMenu = ({ pathname, sidebarExpanded, setSidebarExpanded }) => {
   return (
     <nav className="mt-5 py-2 px-4 lg:mt-9 lg:px-6">
-      {/* Example of menu item */}
-      {/* Other menu items should be added here with similar structure */}
+  
     </nav>
   );
 };
