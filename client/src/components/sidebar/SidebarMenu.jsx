9c01ead2a88efff34ca6434de31df2dc918b985a
josephfox-ch <142756034+josephfox-ch@users.noreply.github.com>
2024-04-30 00:28:32 +0200

    Build: TimeDisplay, HouseSelector,SidebarHeader components

---
+++ client/src/components/sidebar/SidebarMenu.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SidebarLinkGroup from './SidebarLinkGroup';
+
+const SidebarMenu = ({ pathname, sidebarExpanded, setSidebarExpanded }) => {
+  return (
+    <nav className="mt-5 py-2 px-4 lg:mt-9 lg:px-6">
+      {/* Example of menu item */}
+      {/* Other menu items should be added here with similar structure */}
+    </nav>
+  );
+};
+
+export default SidebarMenu;
