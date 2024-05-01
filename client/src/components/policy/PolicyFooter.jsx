93976388e49c4ab915bc02e97967cedd4f3b430b
josephfox-ch <142756034+josephfox-ch@users.noreply.github.com>
2024-05-02 01:50:16 +0200

    Style: Policy Page

--- client/src/components/policy/PolicyFooter.jsx
+++ client/src/components/policy/PolicyFooter.jsx
@@ -1,28 +1,23 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 const PolicyFooter = () => {
   return (
-    <Container
-      className="policy-footer text-center mt-auto py-4 text-white"
-      fluid
-    >
-      <Col>
-        <nav className="policy-footer-nav">
-          <Link className=" mx-2" to="/">
-            Home
-          </Link>
-          <Link className=" mx-2" to="/contact">
-            Contact
-          </Link>
-        </nav>
-      </Col>
-      <Col>
-        <p className="mx-2">smartFOX® Home Systems &copy; {new Date().getFullYear()} </p>
-      </Col>
-    </Container>
+    <div className=" border-t  text-black text-center py-4 mt-auto">
+      <div className="flex justify-center space-x-4">
+        <Link to="/" className="hover:text-gray-300 transition-colors">
+          Home
+        </Link>
+        <Link to="/contact" className="hover:text-gray-300 transition-colors">
+          Contact
+        </Link>
+      </div>
+      <p className="mt-2">
+        SmartFOX® Home Systems &copy; {new Date().getFullYear()}
+      </p>
+    </div>
   );
 };
 
 export default PolicyFooter;
+
