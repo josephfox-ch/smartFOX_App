8b8cdfbcea29763abebe6119978792ee4a2c7e1e
josephfox-ch <142756034+josephfox-ch@users.noreply.github.com>
2024-05-06 21:02:31 +0200

    Build: jwtHelpers - authMiddleware

--- server/src/utils/jwtHelpers.js
+++ server/src/utils/jwtHelpers.js
@@ -1,67 +1,32 @@
 import jwt from "jsonwebtoken";
-import { RefreshToken } from "../api/models/index.js";
-import { Op } from "sequelize";
 import logger from "../config/logger.js";
-
 
 export const generateToken = (user) => {
   const payload = {
     id: user.id,
-    email: user.email
+    email: user.email,
   };
-  return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '180 days' });
+  try {
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
+      expiresIn: process.env.JWT_EXPIRATION,
+    });
+    logger.info(`Token generated for user ${user.id}`);
+    return token;
+  } catch (error) {
+    logger.error(
+      `Error generating token for user ${user.id}: ${error.message}`
+    );
+    throw new Error("Token generation failed.");
+  }
 };
 
 export const verifyToken = (token) => {
   try {
-    return jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    logger.info("Token verified successfully.");
+    return decoded;
   } catch (error) {
+    logger.warn(`Token verification failed: ${error.message}`);
     return null;
   }
 };
-function generateTokens(user) {
-    const accessToken = jwt.sign(
-        { userId: user.id, email: user.email },
-        process.env.ACCESS_TOKEN_SECRET,
-        { expiresIn: process.env.ACCESS_TOKEN_EXPIRATION }
-    );
-
-    const refreshToken = jwt.sign(
-        { userId: user.id },
-        process.env.REFRESH_TOKEN_SECRET,
-        { expiresIn: process.env.REFRESH_TOKEN_EXPIRATION }
-    );
-
-    return { accessToken, refreshToken };
-}
-
-function verifyAccessToken(token) {
-    try {
-        return { isValid: true, payload: jwt.verify(token, process.env.ACCESS_TOKEN_SECRET) };
-    } catch (error) {
-        return { isValid: false, error };
-    }
-}
-
-function verifyRefreshToken(token) {
-    try {
-        return { isValid: true, payload: jwt.verify(token, process.env.REFRESH_TOKEN_SECRET) };
-    } catch (error) {
-        return { isValid: false, error };
-    }
-}
-
-async function cleanExpiredTokens() {
-    await RefreshToken.destroy({
-        where: {
-            expiryDate: { [Op.lt]: new Date() }
-        }
-    });
-}
-
-export { generateTokens, verifyAccessToken, verifyRefreshToken,cleanExpiredTokens};
-
-
-
-
-
