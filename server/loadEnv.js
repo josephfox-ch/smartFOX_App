da36bec6668b1649ac53075307ee5116839fbc9c
josephfox-ch <142756034+josephfox-ch@users.noreply.github.com>
2024-05-06 22:17:10 +0200

    Update: format

--- server/loadEnv.js
+++ server/loadEnv.js
@@ -1,6 +1,1 @@
 import "dotenv/config";
-import "./src/api/models/index.js";
-import "./src/config/passport.js";
-import "./src/config/logger.js";
-import "./src/config/db.js";
-import './src/config/session-config.js'
--- server/src/api/middlewares/errorHandler.js
+++ server/src/api/middlewares/errorHandler.js
@@ -1,12 +1,16 @@
-import logger from '../../config/logger.js';
+import logger from "../../config/logger.js";
 
 const errorHandler = (err, req, res, next) => {
-  logger.error(`${err.status || 500} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
+  logger.error(
+    `${err.status || 500} - ${err.message} - ${req.originalUrl} - ${
+      req.method
+    } - ${req.ip}`
+  );
   res.status(err.status || 500);
   res.json({
     error: {
-      message: err.message || 'An unexpected error occurred.'
-    }
+      message: err.message || "An unexpected error occurred.",
+    },
   });
 };
 
