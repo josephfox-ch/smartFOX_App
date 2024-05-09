6f5c4a77dd2262295e6f12e04d0d0641f9198abe
josephfox-ch <142756034+josephfox-ch@users.noreply.github.com>
2024-05-09 14:50:25 +0200

    Build: Avatar-Edit-Form

---
+++ client/src/components/forms/AvatarEditForm.jsx
@@ -0,0 +1,122 @@
+import React, { useState } from "react";
+import Modal from "react-modal";
+import AvatarEditor from "react-avatar-editor";
+import { useUser } from "../../context/UserContext";
+import userOne from "../../images/user/user-01.png";
+
+Modal.setAppElement("#root");
+
+const AvatarEditForm = () => {
+  const { user } = useUser();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [editor, setEditor] = useState(null);
+
+  const handleUpdateClick = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleDeleteClick = () => {
+
+    console.log("Delete clicked");
+  };
+
+  const handleSaveClick = () => {
+    if (editor) {
+      const canvas = editor.getImageScaledToCanvas().toDataURL();
+      console.log(canvas); 
+    }
+    setIsModalOpen(false);
+  };
+
+  const handleFileChange = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
+
+  return (
+    <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
+      <div className="border-b border-stroke py-4 px-7 dark:border-strokedark">
+        <h3 className="font-medium text-black dark:text-white">Your Photo</h3>
+      </div>
+      <div className="p-7">
+        <form action="#">
+          <div className="mb-4 flex items-center gap-3">
+            <div className="h-14 w-14 rounded-full">
+              <img src={userOne} alt="User" />
+            </div>
+            <div>
+              <span className="mb-1.5 text-black dark:text-white">
+                Edit your photo
+              </span>
+              <span className="flex gap-2.5">
+                <button
+                  className="text-sm hover:text-primary"
+                  type="button"
+                  onClick={handleDeleteClick}
+                >
+                  Delete
+                </button>
+                <button
+                  className="text-sm hover:text-primary"
+                  type="button"
+                  onClick={handleUpdateClick}
+                >
+                  Update
+                </button>
+              </span>
+            </div>
+          </div>
+        </form>
+      </div>
+      <Modal
+        className="fixed inset-0 flex items-center justify-center z-50 "
+        isOpen={isModalOpen}
+        onRequestClose={() => setIsModalOpen(false)}
+        contentLabel="Edit Photo"
+        overlayClassName="fixed inset-0 bg-black bg-opacity-50 backdrop-blur-sm"
+      >
+        <div className="bg-white rounded-lg shadow-lg p-6 w-full max-w-md ">
+          <h2 className="text-center text-lg font-semibold  mb-4 p-2 rounded">
+            Edit Your Photo
+          </h2>
+          <div className="flex flex-col items-center mb-4">
+            <input
+              className="mb-4"
+              type="file"
+              accept="image/*"
+              onChange={handleFileChange}
+            />
+            {selectedFile && (
+              <AvatarEditor
+                ref={(ref) => setEditor(ref)}
+                image={selectedFile}
+                width={250}
+                height={250}
+                border={50}
+                borderRadius={125}
+                scale={1.2}
+              />
+            )}
+          </div>
+
+          <div className="flex justify-end gap-4 mt-4">
+            <button
+              className="border border-stroke py-2 px-6 text-sm text-black hover:shadow-lg hover:bg-gray-100 transition duration-200"
+              onClick={() => setIsModalOpen(false)}
+            >
+              Cancel
+            </button>
+            <button
+              className="bg-blue-600 py-2 px-6 text-sm text-white  hover:bg-blue-700 transition duration-200"
+              onClick={handleSaveClick}
+            >
+              Save
+            </button>
+          </div>
+        </div>
+      </Modal>
+    </div>
+  );
+};
+
+export default AvatarEditForm;
--- client/src/components/forms/PersonalInformationForm.jsx
+++ client/src/components/forms/PersonalInformationForm.jsx
@@ -1,13 +1,18 @@
 import React from "react";
+import { useUser } from "../../context/UserContext";
 import PhoneInput from "react-phone-input-2";
 import "react-phone-input-2/lib/style.css";
-import { useUser } from "../../context/UserContext";
 import { FaRegUser } from "react-icons/fa";
 import { MdOutlineMail } from "react-icons/md";
 import { RiLockPasswordFill } from "react-icons/ri";
 
-function PersonalInformationForm() {
-  const { user } = useUser();
+const PersonalInformationForm = () => {
+  const { user, updateUser, deleteUser } = useUser();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
       <div className="border-b border-stroke py-4 px-7 dark:border-strokedark">
@@ -16,7 +21,7 @@ function PersonalInformationForm() {
         </h3>
       </div>
       <div className="p-7">
-        <form action="#">
+        <form onSubmit={handleSubmit}>
           <div className="mb-5.5 flex flex-col gap-5.5 sm:flex-row">
             <div className="w-full sm:w-1/2">
               <label
@@ -30,12 +35,10 @@ function PersonalInformationForm() {
                   <FaRegUser size="20" />
                 </span>
                 <input
-                  className="w-full  border border-stroke bg-gray py-2 pl-11.5 pr-4.5 text-black focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 dark:focus:border-indigo-500 dark:border-strokedark dark:bg-meta-4 dark:text-white "
+                  className="w-full border border-stroke bg-gray py-2 pl-11.5 pr-4.5 text-black focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 dark:focus:border-indigo-500 dark:border-strokedark dark:bg-meta-4 dark:text-white"
                   type="text"
-                  name="firstName"
                   id="firstName"
-                  placeholder="Your First Name"
-                  defaultValue={user ? `${user.firstName}` : ""}
+                  defaultValue={user ? user.firstName : ""}
                 />
               </div>
             </div>
@@ -51,12 +54,10 @@ function PersonalInformationForm() {
                   <FaRegUser size="20" />
                 </span>
                 <input
-                  className="w-full  border border-stroke bg-gray py-2 pl-11.5 pr-4.5 text-black focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 dark:focus:border-indigo-500 dark:border-strokedark dark:bg-meta-4 dark:text-white"
+                  className="w-full border border-stroke bg-gray py-2 pl-11.5 pr-4.5 text-black focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 dark:focus:border-indigo-500 dark:border-strokedark dark:bg-meta-4 dark:text-white"
                   type="text"
-                  name="lastName"
                   id="lastName"
-                  placeholder="Your Last Name"
-                  defaultValue={user ? `${user.lastName}` : ""}
+                  defaultValue={user ? user.lastName : ""}
                 />
               </div>
             </div>
@@ -74,16 +75,13 @@ function PersonalInformationForm() {
                   <MdOutlineMail size="20" />
                 </span>
                 <input
-                  className="w-full  border border-stroke bg-gray py-2 pl-11.5 pr-4.5 text-black focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 dark:focus:border-indigo-500 dark:border-strokedark dark:bg-meta-4 dark:text-white "
+                  className="w-full border border-stroke bg-gray py-2 pl-11.5 pr-4.5 text-black focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 dark:focus:border-indigo-500 dark:border-strokedark dark:bg-meta-4 dark:text-white"
                   type="email"
-                  name="emailAddress"
                   id="emailAddress"
-                  placeholder="email@example.com"
-                  defaultValue={user ? `${user.email} ` : ""}
+                  defaultValue={user ? user.email : ""}
                 />
               </div>
             </div>
-
             <div className="w-full sm:w-1/2">
               <label
                 className="mb-3 block text-sm font-medium text-black dark:text-white"
@@ -95,46 +93,43 @@ function PersonalInformationForm() {
                 <span className="absolute left-4.5 top-3">
                   <RiLockPasswordFill size="20" />
                 </span>
-
                 <input
-                  className="w-full  border border-stroke bg-gray py-2  pl-11.5 pr-4.5  text-black focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 dark:focus:border-indigo-500 dark:border-strokedark dark:bg-meta-4 dark:text-white"
-                  type="text"
-                  name="password"
+                  className="w-full border border-stroke bg-gray py-2 pl-11.5 pr-4.5 text-black focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 dark:focus:border-indigo-500 dark:border-strokedark dark:bg-meta-4 dark:text-white"
+                  type="password"
                   id="password"
                   placeholder="************"
-                  defaultValue=""
                 />
               </div>
             </div>
           </div>
-
           <div className="mb-5.5">
             <PhoneInput
               international
               countryCallingCodeEditable={false}
               inputStyle={{ width: "100%", border: "1px solid #AEB7C0" }}
               country={"ch"}
-              value={user ? `${user.phoneNumber} ` : ""}
+              value={user ? user.phoneNumber : ""}
               onChange={() => {}}
-              className="flex mt-1 block w-full  rounded  border-bodydark "
+              className="flex mt-1 block w-full rounded border-bodydark"
             />
           </div>
           <div className="flex flex-col-reverse sm:flex-row justify-between items-center gap-4.5">
             <button
-              className="w-full sm:w-auto sm:flex-3 justify-center border border-stroke py-2 px-4 text-sm text-white bg-red-500 hover:shadow-1 hover:bg-red-600 dark:border-strokedark dark:text-white"
+              className="w-full sm:w-auto sm:flex-3 justify-center border border-stroke py-2 px-4 text-sm text-white bg-red-500 hover:shadow-1 hover:bg-red-600 dark:border-strokedark dark:text-white hover:shadow-lg"
               type="button"
+              onClick={deleteUser}
             >
               Delete Account
             </button>
             <div className="flex justify-between gap-4.5">
               <button
-                className="  justify-center border border-stroke py-2 px-6 text-sm text-black hover:shadow-1 hover:bg-bodydark dark:border-strokedark dark:text-white"
+                className="justify-center border border-stroke py-2 px-6 text-sm text-black hover:shadow-1 hover:bg-bodydark dark:border-strokedark dark:text-white hover:shadow-lg"
                 type="button"
               >
                 Cancel
               </button>
               <button
-                className="  justify-center bg-blue-600 py-2 px-6 text-sm text-white hover:bg-blue-700"
+                className="justify-center bg-blue-600 py-2 px-6 text-sm text-white hover:bg-blue-700 hover:shadow-lg"
                 type="submit"
               >
                 Save
@@ -145,7 +140,6 @@ function PersonalInformationForm() {
       </div>
     </div>
   );
-}
+};
 
 export default PersonalInformationForm;
-
