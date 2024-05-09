d603ec25305f3e95c88d9fdba43daac34f539612
josephfox-ch <142756034+josephfox-ch@users.noreply.github.com>
2024-05-09 14:50:25 +0200

    Build: Avatar-Edit-Form

--- client/src/pages/AccountSettings.jsx
+++ client/src/pages/AccountSettings.jsx
@@ -1,24 +1,18 @@
+import React from "react";
 import Breadcrumb from "../components/Breadcrumb";
 import PersonalInformationForm from "../components/forms/PersonalInformationForm";
-import PhotoUploadForm from "../components/forms/PhotoUploadForm";
-import { useUser } from "../context/UserContext";
-
+import AvatarEditForm from "../components/forms/AvatarEditForm";
 
 const AccountSettings = () => {
-  const { user } = useUser();
   return (
     <div className="mx-auto max-w-7xl">
       <Breadcrumb className="text-foxColor" pageName="Account Settings" />
-
       <div className="grid grid-cols-1 xl:grid-cols-5 gap-8">
-        {/* Personal Information Form */}
-        <div className="col-span-1 xl:col-span-3 order-2 xl:order-1">
-          <PersonalInformationForm />
+        <div className="col-span-1 xl:col-span-2 order-1 xl:order-2 ">
+          <AvatarEditForm />
         </div>
-
-        {/* Photo Section */}
-        <div className="col-span-1 xl:col-span-2 order-1 xl:order-2">
-          <PhotoUploadForm />
+        <div className="col-span-1 xl:col-span-3 order-2 xl:order-1 ">
+          <PersonalInformationForm />
         </div>
       </div>
     </div>
