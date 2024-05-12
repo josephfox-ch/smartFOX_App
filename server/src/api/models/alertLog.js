481b7887ec0d7e006a9b3df82d074a48c580c32f
josephfox-ch <142756034+josephfox-ch@users.noreply.github.com>
2024-05-12 23:31:11 +0200

    build: hvacsystemlog, alertlog, room, lihgtingcontrol models

---
+++ server/src/api/models/alertLog.js
@@ -0,0 +1,29 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../../config/db.js";
+
+const AlertLog = sequelize.define("AlertLog", {
+  alertId: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true,
+  },
+  homeId: {
+    type: DataTypes.UUID,
+    allowNull: false,
+    references: {
+      model: "Homes",
+      key: "id",
+    }
+  },
+  message: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  severity: {
+    type: DataTypes.ENUM,
+    values: ['low', 'medium', 'high', 'critical'],
+    allowNull: false,
+  }
+});
+
+export default AlertLog;
---
+++ server/src/api/models/hvacSystemLog.js
@@ -0,0 +1,33 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../../config/db.js";
+
+const HVACSystemLog = sequelize.define("HVACSystemLog", {
+  logId: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true,
+  },
+  homeId: {
+    type: DataTypes.UUID,
+    allowNull: false,
+    references: {
+      model: "Homes",
+      key: "id",
+    }},
+  status: {
+    type: DataTypes.ENUM,
+    values: ['on', 'off', 'maintenance', 'error'],
+    allowNull: false,
+  },
+  startedAt: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: DataTypes.NOW,
+  },
+  endedAt: {
+    type: DataTypes.DATE,
+    allowNull: true,
+  },
+});
+
+export default HVACSystemLog;
