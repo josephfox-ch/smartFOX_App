c96136bea627925405387d9b803a101861ba47eb
josephfox-ch <142756034+josephfox-ch@users.noreply.github.com>
2024-05-14 19:48:21 +0200

    refactor: home selector

--- client/src/components/sidebar/home/MyHomeButton.jsx
+++ client/src/components/sidebar/home/MyHomeButton.jsx
@@ -1,22 +1,20 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import { FaHouseUser } from "react-icons/fa";
+import HomeSelector from "./HomeSelector";
 
 function MyHomeButton() {
   return (
     <>
-      <div className="flex flex-col items-center mb-4 duration-300 ease-in-out text-bodydark1 hover:text-foxColor ">
+      <div className="flex flex-col items-center mb-2 duration-300 ease-in-out text-bodydark1 hover:text-foxColor ">
         <NavLink
           to="/dashboard/my-home"
           className="flex flex-col items-center text-center focus:text-foxColor"
         >
-          <FaHouseUser
-            size="80"
-            className="cursor-pointer   "
-          />
-          <span className="mt-2 text-sm">My Home</span>
+          <FaHouseUser size="80" className="cursor-pointer   " />
         </NavLink>
       </div>
+      <HomeSelector />
     </>
   );
 }
