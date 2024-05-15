0fcfadf3c137d35439406e56a12f7fbb6eeb648b
josephfox-ch <142756034+josephfox-ch@users.noreply.github.com>
2024-05-16 00:34:34 +0200

    build: weather info widget

--- client/src/context/WeatherContext.jsx
+++ client/src/context/WeatherContext.jsx
@@ -7,6 +7,11 @@ const WeatherContext = createContext();
 export const WeatherProvider = ({ children }) => {
   const { selectedHome } = useHomes();
   const [outdoorTemperature, setOutdoorTemperature] = useState(null);
+  const [humidity,setHumidity] = useState(null);
+  const [windSpeed,setWindSpeed] = useState(null);
+  const [weatherDescription,setWeatherDescription] = useState(null);
+  const [weatherType,setWeatherType] = useState(null);
+  const [weatherIcon,setWeatherIcon] = useState(null);
 
   useEffect(() => {
     const fetchWeather = async () => {
@@ -17,6 +22,12 @@ export const WeatherProvider = ({ children }) => {
             longitude: selectedHome.longitude,
           });
           setOutdoorTemperature(weatherData.main.temp);
+          setHumidity(weatherData.main.humidity);
+          setWindSpeed(weatherData.wind.speed);
+          setWeatherDescription(weatherData.weather[0].description);
+          setWeatherType(weatherData.weather[0].main);
+          setWeatherIcon(weatherData.weather[0].icon);
+          console.log("Weather data fetched:", weatherData);
         } catch (error) {
           console.error("Error fetching weather data:", error);
         }
@@ -28,7 +39,7 @@ export const WeatherProvider = ({ children }) => {
   }, [selectedHome]);
 
   return (
-    <WeatherContext.Provider value={{ outdoorTemperature }}>
+    <WeatherContext.Provider value={{ outdoorTemperature ,humidity,windSpeed,weatherDescription,weatherType,weatherIcon }}>
       {children}
     </WeatherContext.Provider>
   );
