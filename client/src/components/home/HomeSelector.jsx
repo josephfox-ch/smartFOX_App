164e9de35039c9ffa50c6a628dfd6b3624479140
josephfox-ch <142756034+josephfox-ch@users.noreply.github.com>
2024-05-16 22:54:52 +0200

    refactor: folder structure

--- client/src/components/home/HomeSelector.jsx
+++ client/src/components/home/HomeSelector.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useHomes } from "../../../context/HomeContext";
+import { useHomes } from "../../context/HomeContext";
 
 const HomeSelector = () => {
   const { homes, selectedHome, selectHome, loading, error } = useHomes();
