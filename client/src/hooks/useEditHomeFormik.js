ff2a0901ba8b413eec738a14e421fe7c017bd9c9
josephfox-ch <142756034+josephfox-ch@users.noreply.github.com>
2024-05-20 13:17:34 +0200

    build: get home details - edit home form

---
+++ client/src/hooks/useEditHomeFormik.js
@@ -0,0 +1,93 @@
+import { useFormik } from "formik";
+import {
+  homeValidationSchema,
+  energyCertificateValidationSchema,
+} from "../components/forms/validationSchemas";
+import { updateHomeWithEnergyCertificate } from "../api/services/homeService";
+import { useNavigate } from "react-router-dom";
+import { useHomes } from "../context/HomeContext";
+import * as Yup from "yup";
+import { useAlert } from "../context/AlertContext";
+import { formatDate } from "../utils/utils";
+
+const combinedValidationSchema = Yup.object().shape({
+  ...homeValidationSchema.fields,
+  ...energyCertificateValidationSchema.fields,
+});
+
+const useEditHomeFormik = (homeId) => {
+  const { showAlert } = useAlert();
+  const navigate = useNavigate();
+  const { fetchHomes, selectedHome, fetchHomeDetails } = useHomes();
+
+  const formik = useFormik({
+    initialValues: {
+      name: selectedHome?.name || "",
+      streetAddress: selectedHome?.streetAddress || "",
+      city: selectedHome?.city || "",
+      country: selectedHome?.country || "CH",
+      postalCode: selectedHome?.postalCode || "",
+      timeZone: selectedHome?.timeZone || "Europe/Zurich",
+      latitude: selectedHome?.latitude || "",
+      longitude: selectedHome?.longitude || "",
+      buildingVolume: selectedHome?.EnergyCertificate?.buildingVolume || "",
+      heatLossCoefficient:
+        selectedHome?.EnergyCertificate?.heatLossCoefficient || "",
+      constructionYear: selectedHome?.EnergyCertificate?.constructionYear || "",
+      renewalDate:
+        formatDate(selectedHome?.EnergyCertificate?.renewalDate) || "",
+      globalHeatLossCoefficient:
+        selectedHome?.EnergyCertificate?.globalHeatLossCoefficient || "",
+      volumeOfHeatedZone:
+        selectedHome?.EnergyCertificate?.volumeOfHeatedZone || "",
+      heatEmissionCoefficient:
+        selectedHome?.EnergyCertificate?.heatEmissionCoefficient || "",
+      freeHeatGains: selectedHome?.EnergyCertificate?.freeHeatGains || "",
+    },
+    validationSchema: combinedValidationSchema,
+    onSubmit: async (values, { setSubmitting }) => {
+      try {
+        const {
+          name,
+          streetAddress,
+          city,
+          country,
+          postalCode,
+          timeZone,
+          latitude,
+          longitude,
+          ...energyCertificateData
+        } = values;
+        const homeData = {
+          name,
+          streetAddress,
+          city,
+          country,
+          postalCode,
+          timeZone,
+          latitude,
+          longitude,
+        };
+
+        const updatedHome = await updateHomeWithEnergyCertificate(
+          homeId,
+          homeData,
+          energyCertificateData
+        );
+        console.log("Home updated:", updatedHome);
+        await fetchHomes();
+        showAlert("success", "Success", `Home ${updatedHome.name} updated`);
+        navigate("/dashboard/my-home", { replace: true });
+      } catch (error) {
+        console.error("Error updating home:", error);
+        showAlert("error", "Error", "Could not update the home");
+      } finally {
+        setSubmitting(false);
+      }
+    },
+  });
+
+  return formik;
+};
+
+export default useEditHomeFormik;
--- server/src/api/services/homeService.js
+++ server/src/api/services/homeService.js
@@ -1,4 +1,4 @@
-import { Home, EnergyCertificate,ClimateControl } from "../models/index.js";
+import { Home, ClimateControl, EnergyCertificate, Device, Event, EnergyUsage, TemperatureRecord, HVACSystemLog, AlertLog, Room, LightingControl, RoomTemperature, LightingReport } from "../models/index.js";
 import sequelize from "../../config/db.js";
 import logger from "../../config/logger.js";
 
@@ -15,102 +15,52 @@ export const getHomes = async (userId) => {
 export const createHomeWithEnergyCertificate = async (userId, homeData, energyCertificateData) => {
   const transaction = await sequelize.transaction();
   try {
-    const {
-      name,
-      streetAddress,
-      city,
-      country,
-      postalCode,
-      timeZone,
-      latitude,
-      longitude,
-    } = homeData;
+    const { name, streetAddress, city, country, postalCode, timeZone, latitude, longitude } = homeData;
 
-    const newHome = await Home.create(
-      {
-        userId,
-        name,
-        streetAddress,
-        city,
-        country,
-        postalCode,
-        timeZone,
-        latitude,
-        longitude,
-      },
-      { transaction }
-    );
+    const newHome = await Home.create({ userId, name, streetAddress, city, country, postalCode, timeZone, latitude, longitude }, { transaction });
 
     logger.info(`Home created for user ${userId}: ${newHome.id}`);
 
-    const newEnergyCertificate = await EnergyCertificate.create(
-      {
-        homeId: newHome.id,
-        ...energyCertificateData,
-      },
-      { transaction }
-    );
+    const newEnergyCertificate = await EnergyCertificate.create({ homeId: newHome.id, ...energyCertificateData }, { transaction });
 
-    logger.info(
-      `Energy certificate created for home ${newHome.id}: ${newEnergyCertificate.id}`
-    );
+    logger.info(`Energy certificate created for home ${newHome.id}: ${newEnergyCertificate.id}`);
 
-    
-    const defaultClimateControl = {
-      homeId: newHome.id,
-      desiredTemperature: 22,
-      currentTemperature: 22,
-      mode: 'away',
-    };
+    const defaultClimateControl = { homeId: newHome.id, desiredTemperature: 22, currentTemperature: 22, mode: 'away' };
 
     const newClimateControl = await ClimateControl.create(defaultClimateControl, { transaction });
 
     logger.info(`ClimateControl created for home ${newHome.id}: ${newClimateControl.id}`);
 
     await transaction.commit();
-    return {
-      ...newHome.toJSON(),
-      energyCertificate: newEnergyCertificate,
-      climateControl: newClimateControl,
-    };
+    return { ...newHome.toJSON(), energyCertificate: newEnergyCertificate, climateControl: newClimateControl };
   } catch (error) {
     await transaction.rollback();
-    logger.error(
-      `Error creating home, energy certificate, and climate control for user ${userId}: ${error.message}`
-    );
+    logger.error(`Error creating home, energy certificate, and climate control for user ${userId}: ${error.message}`);
     throw new Error('Could not create home, energy certificate, and climate control');
   }
 };
 
-export const updateHome = async (userId, homeId, homeData) => {
+export const updateHomeWithEnergyCertificate = async (userId, homeId, homeData, energyCertificateData) => {
+  const transaction = await sequelize.transaction();
   try {
-    const { houseName, streetAddress, city, country, postalCode, timeZone } =
-      homeData;
-    const [updated] = await Home.update(
-      {
-        houseName,
-        streetAddress,
-        city,
-        country,
-        postalCode,
-        timeZone,
-      },
-      {
-        where: {
-          id: homeId,
-          userId,
-        },
-      }
-    );
+    const { name, streetAddress, city, country, postalCode, timeZone, latitude, longitude } = homeData;
+
+    const [updated] = await Home.update({ name, streetAddress, city, country, postalCode, timeZone, latitude, longitude }, { where: { id: homeId, userId }, transaction });
 
     if (!updated) {
       throw new Error("Home not found or user unauthorized");
     }
 
-    const updatedHome = await Home.findOne({ where: { id: homeId } });
-    logger.info(`Home updated for user ${userId}: ${homeId}`);
+    await EnergyCertificate.update({ ...energyCertificateData }, { where: { homeId }, transaction });
+
+    logger.info(`Home and energy certificate updated for user ${userId}: ${homeId}`);
+
+    const updatedHome = await Home.findOne({ where: { id: homeId }, include: [ClimateControl, EnergyCertificate, Device, Event, EnergyUsage, TemperatureRecord, HVACSystemLog, AlertLog, Room, LightingControl, RoomTemperature, LightingReport], transaction });
+
+    await transaction.commit();
     return updatedHome;
   } catch (error) {
+    await transaction.rollback();
     logger.error(`Error updating home for user ${userId}: ${error.message}`);
     throw new Error("Could not update home");
   }
@@ -118,12 +68,7 @@ export const updateHome = async (userId, homeId, homeData) => {
 
 export const deleteHome = async (userId, homeId) => {
   try {
-    const deleted = await Home.destroy({
-      where: {
-        id: homeId,
-        userId,
-      },
-    });
+    const deleted = await Home.destroy({ where: { id: homeId, userId } });
 
     if (!deleted) {
       throw new Error("Home not found or user unauthorized");
@@ -136,3 +81,20 @@ export const deleteHome = async (userId, homeId) => {
     throw new Error("Could not delete home");
   }
 };
+
+export const getHomeDetails = async (userId, homeId) => {
+  try {
+    const home = await Home.findOne({
+      where: { id: homeId, userId },
+      include: [ClimateControl, EnergyCertificate, Device, EnergyUsage, TemperatureRecord, HVACSystemLog, AlertLog, Room, LightingControl, LightingReport]
+    });
+    if (!home) {
+      throw new Error("Home not found or user unauthorized");
+    }
+    return home;
+  } catch (error) {
+    logger.error(`Error fetching home details for user ${userId}: ${error.message}`);
+    throw new Error("Could not fetch home details");
+  }
+};
+
