68299234245d078b3d176b6a75ad8c5bf311d6b8
josephfox-ch <142756034+josephfox-ch@users.noreply.github.com>
2024-05-22 17:04:38 +0200

    build: access control routes

--- server/src/api/routes/routes.js
+++ server/src/api/routes/routes.js
@@ -5,6 +5,7 @@ import awsRoutes from "./awsRoutes.js";
 import climateControlRoutes from './climateControlRoutes.js'
 import hvacSystemLogRoutes from "./hvacSystemLogRoutes.js";
 import energyUsageRoutes from "./energyUsageRoutes.js";
+import accessControlRoutes from "./accessControlRoutes.js";
 
 export const useRoutes = (app) => {
   app.use("/api/v1/auth", authRoutes);
@@ -14,4 +15,5 @@ export const useRoutes = (app) => {
   app.use("/api/v1/climate-control", climateControlRoutes);
   app.use("/api/v1/hvac-system-log", hvacSystemLogRoutes);
   app.use("/api/v1/energy-usage", energyUsageRoutes);
+  app.use("/api/v1/access-control", accessControlRoutes);
 };
