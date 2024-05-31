cae248242bd62e8f4581b3dddc4e49676b5f1046
josephfox-ch <josephfox.ch@gmail.com>
2024-06-01 01:40:14 +0200

    refactor: edit home with energy-certificate

--- client/src/components/widgets/EnergyMonitor.jsx
+++ client/src/components/widgets/EnergyMonitor.jsx
@@ -64,7 +64,7 @@ const EnergyMonitor = () => {
   };
 
   return (
-    <div className="bg-gradient-to-br from-gray-300 to-orange-200 dark:bg-gradient-to-br dark:from-gray-600 dark:to-gray-400  p-8 rounded-lg shadow-lg flex flex-col items-center space-y-6 text-gray-800 dark:text-gray-100 transition-transform transform hover:scale-105  h-full">
+    <div className="bg-gradient-to-br from-gray-100 to-orange-300 dark:bg-gradient-to-br dark:from-gray-600 dark:to-gray-400  p-8 rounded-lg shadow-lg flex flex-col items-center space-y-6 text-gray-800 dark:text-gray-100 transition-transform transform hover:scale-105  h-full">
       <h3 className="text-2xl font-bold mb-4 text-center mb-4 text-gray-800 dark:text-gray-100 text-center ">
         Energy Monitor
       </h3>
