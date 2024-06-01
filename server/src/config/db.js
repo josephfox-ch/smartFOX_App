ae7f9f12843b0e94a1af77a934194c8a499d1f9b
josephfox-ch <josephfox.ch@gmail.com>
2024-06-01 02:34:09 +0200

    refactor: db.js

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,20 +1,27 @@
-import Sequelize from "sequelize";
+import { Sequelize } from "sequelize";
 import logger from "./logger.js";
+import mysql2 from "mysql2";
+
+const options = {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: process.env.DB_DIALECT,
+  logging: (msg) => {
+    if (msg.includes("Error")) {
+      console.log(msg);
+    }
+  },
+};
+
+if (options.dialect === 'mysql') {
+  options.dialectModule = mysql2;
+}
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: process.env.DB_DIALECT,
-    logging: (msg) => {
-      if (msg.includes("Error")) {
-        console.log(msg);
-      }
-    },
-  }
+  options
 );
 
 export const connectDB = async () => {
@@ -22,8 +29,8 @@ export const connectDB = async () => {
     await sequelize.authenticate();
     logger.info("Database connection has been established successfully.");
     await sequelize.sync({
-      force: false,
-      alter: true,
+      force: process.env.NODE_ENV === 'development' && process.env.SEQUELIZE_FORCE === 'true',
+      alter: process.env.NODE_ENV !== 'production' && process.env.SEQUELIZE_ALTER === 'true',
     });
     logger.info("Database has been synced.");
   } catch (error) {
